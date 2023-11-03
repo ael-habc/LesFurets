d2b7958cd405c8ac69e742bbd8b342a6b4ca79ee
Abdellah El Habchi <aelhabchi@rightplacecall.local>
2023-11-03 10:45:39 +0100

    stable version

--- src/App.js
+++ src/App.js
@@ -1,54 +1,86 @@
 import React, { useState } from "react";
 
+//Assets (Images)
 import logo from "./assests/images/logo2.png";
 
+//Json Files (Data)
 import questions from "./assests/questions/questions.json";
 
+//Components (UI)
 import Question from "./components/Question";
 
+//Material Tailwind (UI)
+import { Progress } from "@material-tailwind/react";
+
+//Styles (UI)
 import "./App.css";
+
+//Icons (UI)
+import { BiLeftArrowAlt } from "react-icons/bi";
 
 function App() {
   const [number, setNumber] = useState(0);
   const [end, setEnd] = useState(false);
 
+  // const [progressbar1, setProgressbar1] = useState(0);
+  // const [progressbar2, setProgressbar2] = useState(progressbar1 - 100);
+  // const [progressbar3, setProgressbar3] = useState(progressbar2 - 100);
+
   return (
-    <>
-      <nav className="flex p-5 pl-8 text-neutral-500 shadow-lg hover:text-neutral-700 focus:text-neutral-700 dark:bg-neutral-600 lg:py-4">
+    <div className="flex flex-col justify-center items-center w-screen">
+      <nav className="flex p-5 pl-8 text-neutral-500 shadow-lg hover:text-neutral-700 focus:text-neutral-700 dark:bg-neutral-600 lg:py-4 w-screen">
         <a href="/" className="h-auto">
           <img src={logo} alt="logo" className="w-52" />
         </a>
       </nav>
-      <Question question={questions.question[number]} />
-
-      <hr />
-
-      <div className="flex justify-between">
+      <div className="flex flex-col w-2/3 mt-5 justify-center items-center">
+        <div className=" flex w-full justify-between">
+          <Progress
+            value={100}
+            color="blue"
+            className="h-3 w-1/3 rounded-full mb-5"
+            style={{
+              width: `${((number + 1) / questions.question.length) * 100}%`,
+              transition: "width 0.5s ease-in-out",
+              alignSelf: "flex-start",
+            }}
+          />
+         
+        </div>
+        <div className="w-full max-w-2xl mx-auto pt-4 text-center text-customGray text-lg relative">
+          {questions.question[number].title}
+        </div>
         {number > 0 && (
           <button
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            className="flex text-buttonGray items-center justify-center border-buttonGray border-2 rounded-lg w-32 h-14 mt-5 self-start gap-2 font-medium hover:shadow-lg mb-11"
             onClick={() => setNumber(number - 1)}
           >
-            Previous
+            <BiLeftArrowAlt/> Previous
           </button>
         )}
-        {number < questions.question.length - 1 && (
-          <button
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-            onClick={() => setNumber(number + 1)}
-          >
-            Next
-          </button>
+        
+
+        <Question question={questions.question[number]} />
+        <hr />
+        <div className="flex justify-between">
+          {number < questions.question.length - 1 && (
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              onClick={() => setNumber(number + 1)}
+            >
+              Next
+            </button>
+          )}
+        </div>
+        {end && (
+          <div className="flex justify-center">
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+              Submit
+            </button>
+          </div>
         )}
       </div>
-      {end && (
-        <div className="flex justify-center">
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-            Submit
-          </button>
-        </div>
-      )}
-    </>
+    </div>
   );
 }
 
--- src/components/Question.js
+++ src/components/Question.js
@@ -1,12 +1,13 @@
 import React from "react";
 import "./Question.css";
+import { Select, Option } from "@material-tailwind/react";
 
 export default function Question(props) {
   const question = props.question;
   let type = question.question_type;
 
   return (
-    <div className="border m-6">
+    <div className="">
       {type === "text" && (
         <div>
           <h1>{question.question}</h1>
@@ -16,20 +17,22 @@ export default function Question(props) {
       )}
       {type === "select" && (
         <div>
-          <h1>{question.question}</h1>
-          {console.log(question)}
-          <select>
+          <h1 className="text-textBlue text-xl">{question.question}</h1>
+          <Select label="Select Version">
             {question.reponses.map((reponse, index) => (
-              <option key={index} value={reponse}>
+              <Option key={index} value={reponse}>
                 {reponse}
-              </option>
+              </Option>
             ))}
-          </select>
+          </Select>
+          <button className="rounded-xl bg-gradient-to-br from-[#FFC837] to-[#FF6108] px-5 py-3 text-base font-medium text-white transition duration-200 hover:shadow-lg hover:shadow-[#FFC837]/50">
+            Button 9
+          </button>
         </div>
       )}
       {type === "email" && (
         <div>
-          <h1>{question.question}</h1>
+          <h1 className="text-xl">{question.question}</h1>
           <p>{question.hint}</p>
           <input type="email" />
         </div>
